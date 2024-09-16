7c990b2ffcdae1063f902f3851e626f88c67da1e
samuel-edmund-morgan <samuel.edmund.morgan@nasbu.edu.ua>
2024-09-16 20:14:22 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,39 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestRestoreNames:
+    @pytest.fixture()
+    def broken_users_template(self) -> list[dict]:
+        return [
+            {
+                "first_name": None,
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+
+    @pytest.fixture()
+    def repaired_users_template(self) -> list[dict]:
+        return [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+            {
+                "first_name": "Mike",
+                "last_name": "Adams",
+                "full_name": "Mike Adams",
+            },
+        ]
+
+    def test_restore_names(self,
+                           broken_users_template: list[dict],
+                           repaired_users_template: list[dict]) -> None:
+        restore_names(broken_users_template)
+        assert broken_users_template == repaired_users_template
